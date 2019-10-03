de15d4acca121fbf1fbcf706e642daac85c6dc25
Pedro Carvalho <incude@gmail.com>
2019-10-03 23:15:06 +0100

    Wrote KafkaServerStartable initial test.

---
+++ src/test/java/org/pdcarv/embeddedkafka/KafkaServerStartableTest.java
@@ -0,0 +1,32 @@
+package org.pdcarv.embeddedkafka;
+
+import org.junit.Test;
+
+import kafka.metrics.KafkaMetricsReporter;
+import kafka.server.KafkaConfig;
+import kafka.utils.VerifiableProperties;
+import scala.collection.Seq;
+
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+/**
+ * Unit test for simple App.
+ */
+public class KafkaServerStartableTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void KafkaServerStartable_StartsDaemon() {
+        Properties props = TestUtils.createBrokerConfig(0, 8181);
+        props.put(KafkaConfig.LogRetentionTimeHoursProp(), "1");
+        KafkaConfig config = KafkaConfig.fromProps(props);
+        Seq<KafkaMetricsReporter> metrics = KafkaMetricsReporter.startReporters(new VerifiableProperties(props));
+        KafkaServerStartable server = new KafkaServerStartable(config, metrics);
+
+        server.startup();
+
+    }
+}
---
+++ src/test/java/org/pdcarv/embeddedkafka/TestUtils.java
@@ -0,0 +1,35 @@
+package org.pdcarv.embeddedkafka;
+
+import java.io.File;
+import java.util.Properties;
+import java.util.Random;
+
+public class TestUtils 
+{
+    public static Properties createBrokerConfig(Integer nodeId, Integer port) 
+    {
+        Properties props = new Properties();
+        props.put("brokerid", nodeId.toString());
+        props.put("port", port.toString());
+        props.put("log.dir", TestUtils.tempDir().getAbsolutePath());
+        props.put("log.flush.interval", "1");
+        props.put("zookeeper.connect", TestUtils.zookeeperConnect());
+
+        return props;
+    }
+
+    public static File tempDir() 
+    {
+        String ioDir = System.getProperty("java.io.tmpdir");
+        File f = new File(ioDir, "kafka-" + new Random().nextInt(1000000));
+        f.mkdirs();
+        f.deleteOnExit();
+
+        return f;
+      }
+
+    public static String zookeeperConnect() 
+    {
+        return "127.0.0.1:2182";
+    }
+}
